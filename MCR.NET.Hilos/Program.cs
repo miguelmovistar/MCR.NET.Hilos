e8fe4d0d7a07ab31c8c23ccb53fbe522c03a43d9
Miguel Castillo <miguelcastilloenlinea@gmail.com>
2025-05-13 12:52:43 -0600

    Stop thread with while variable

--- MCR.NET.Hilos/Program.cs
+++ MCR.NET.Hilos/Program.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MCR.NET.Hilos
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            // Dos
+
+            bool stopped = false;
+            Thread thread = new Thread(new ThreadStart(() => {
+                while (!stopped)
+                {
+                    Console.WriteLine("Corriendo..." + stopped);
+                    Thread.Sleep(1000);
+                }
+            }));
+
+            thread.Start();
+            Console.WriteLine("Presione una tecla para salir.");
+            Console.ReadKey();
+            stopped = true;
+            thread.Join();
         }
     }
 }
